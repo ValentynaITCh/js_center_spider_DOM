e0e4b8bbcc13bfbde26e6d1d070ed6410e8e64af
Valentyna Chernova <valentyna19960410@gmail.com>
2025-11-28 20:17:40 +0200

    make build

---
+++ index.e8715a1a.js
@@ -0,0 +1,2 @@
+window.addEventListener("load",function(){var e=document.querySelector(".wall"),t=document.querySelector(".spider"),n=e.getBoundingClientRect(),i=t.getBoundingClientRect(),d=(n.height-i.height)/2;t.style.top=d+"px";var l=(n.width-i.width)/2;t.style.left=l+"px"});
+//# sourceMappingURL=index.e8715a1a.js.map
