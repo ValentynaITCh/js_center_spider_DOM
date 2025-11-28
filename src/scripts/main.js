a7573a0bb078c1b5296a64cf30601ef84ad09bf0
Valentyna Chernova <valentyna19960410@gmail.com>
2025-11-28 20:14:24 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
 // write code here
+window.addEventListener('load', () => {
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const wallCords = wall.getBoundingClientRect();
+const spiderCords = spider.getBoundingClientRect();
+const top = (wallCords.height  - spiderCords.height) / 2;
+spider.style.top = top + 'px';
+const left = (wallCords.width - spiderCords.width) / 2;
+spider.style.left = left + 'px';
+});
